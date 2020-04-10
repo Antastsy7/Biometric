1815dd84c1644c5cd3014344a2738365020f2500
Antastsy7 <664692054@qq.com>
2020-04-10 12:20:15 -0400

    Painting

---
+++ Assets/GestureController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestureController : MonoBehaviour, KinectGestures.GestureListenerInterface
+{
+
+    public LineRenderer lineRenderer;
+    
+    private bool flag, grip;
+    private long userId;
+    private List<Vector3> movePath;
+    private Vector3 jointPos;
+    private KinectInterop.JointType joint;
+    private KinectManager manager;
+    private InteractionManager.HandEventType lastHandEvent = InteractionManager.HandEventType.None;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = KinectManager.Instance;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (flag && grip)
+        {
+            userId = manager.GetPrimaryUserID();
+            jointPos = manager.GetJointPosition(userId, (int)KinectInterop.JointType.HandRight);
+            jointPos.z = 0;
+            movePath.Add(jointPos);
+            lineRenderer.positionCount = movePath.Count;
+            lineRenderer.SetPositions(movePath.ToArray());
+        }
+    }
+
+    public bool GestureCancelled(long userId, int userIndex, KinectGestures.Gestures gesture,
+        KinectInterop.JointType joint)
+    {
+        return true;
+    }
+
+    public bool GestureCompleted(long userId, int userIndex, KinectGestures.Gestures gesture,
+        KinectInterop.JointType joint, Vector3 screenPos)
+    {
+        return true;
+    }
+
+    public void GestureInProgress(long userId, int userIndex, KinectGestures.Gestures gesture, float progress,
+        KinectInterop.JointType joint, Vector3 screenPos)
+    {
+        
+    }
+
+    public void UserDetected(long userId, int userIndex)
+    {
+        flag = true;
+        movePath = new List<Vector3>();
+        lineRenderer.positionCount = 0;
+        
+        
+    }
+
+    public void UserLost(long userId, int userIndex)
+    {
+        flag = false;
+        
+    }
+
+    public void toggle(bool b)
+    {
+        if (b)
+        {
+            grip = true;
+        }
+        else
+        {
+            grip = false;
+            lineRenderer.positionCount = 0;
+            movePath.Clear();
+        }
+    }
+}
---
+++ Assets/HandManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandManager : MonoBehaviour, InteractionListenerInterface
+{
+    
+    public InteractionManager interactionManager;
+
+    public GestureController controller;
+    // hand interaction variables   手的交互变量
+    private InteractionManager.HandEventType lastHandEvent = InteractionManager.HandEventType.None;
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactionManager = InteractionManager.Instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void HandGripDetected(long userId, int userIndex, bool isRightHand, bool isHandInteracting, Vector3 handScreenPos)
+    {
+        if (!isHandInteracting || !interactionManager)
+            return;
+        if (userId != interactionManager.GetUserID())
+            return;
+        controller.SendMessage("toggle", true);
+        lastHandEvent = InteractionManager.HandEventType.Grip;
+        //isLeftHandDrag = !isRightHand;
+    }
+
+    public void HandReleaseDetected(long userId, int userIndex, bool isRightHand, bool isHandInteracting, Vector3 handScreenPos)
+    {
+        if (!isHandInteracting || !interactionManager)
+            return;
+        if (userId != interactionManager.GetUserID())
+            return;
+
+        lastHandEvent = InteractionManager.HandEventType.Release;
+        controller.SendMessage("toggle", false);
+        //isLeftHandDrag = !isRightHand;
+    }
+    public bool HandClickDetected(long userId, int userIndex, bool isRightHand, Vector3 handScreenPos)
+    {
+        return true;
+    }
+
+}
